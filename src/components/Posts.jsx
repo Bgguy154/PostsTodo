12bb01ef1569cb86bf92dae246a8bbce8b0295b9
Tanmay Tapre <tanmaytapre12345@gmail.com>
2026-04-20 19:42:24 +0530

    edit functionality added

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,26 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'   // ✅ added useState
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { deletePost } from '../features/fetchSlice'
+import { deletePost, editPost } from '../features/fetchSlice'  // ✅ added editPost
 import Input from './Input'
 
 const Posts = () => {
   const posts=useSelector((state)=>state.fetchData.posts)
   const dispatch=useDispatch();
-         console.log(posts)
+
+  // ✅ added state
+  const [editId,setEditId]=useState(null)
+  const [editText,setEditText]=useState("")
+
   return (
     <>
-       <Input/>
-     {
-    posts.map((item,idx)=>(
-      <div style={{display:"flex",justifyContent:"center",boxSizing:"border-box",border:"1px solid black",height:"150px",width:"700px",alignItems:"center",flexDirection:"column",gap:"10px"}} key={item.id}>
-      <div id={idx} style={{height:"50px",width:"300px"}}>{item.title}</div>
-      <Link to={`${item.id}`}>View Details</Link>
-    <button onClick={()=>dispatch(deletePost(item.id))}>Delete Post</button>
-      </div>
-))
-  }
-  </>
+      <Input/>
+
+      {
+        posts.map((item,idx)=>(
+          <div
+            key={item.id}
+            style={{
+              display:"flex",
+              justifyContent:"center",
+              border:"1px solid black",
+              height:"150px",
+              width:"700px",
+              alignItems:"center",
+              flexDirection:"column",
+              gap:"10px"
+            }}
+          >
+
+            {/* ✅ small conditional change */}
+            {editId === item.id ? (
+              <>
+                <input
+                  value={editText}
+                  onChange={(e)=>setEditText(e.target.value)}
+                />
+                <button onClick={()=>{
+                  dispatch(editPost({id:item.id,title:editText}))
+                  setEditId(null)
+                }}>
+                  Save
+                </button>
+              </>
+            ) : (
+              <>
+                <div>{item.title}</div>
+                <button onClick={()=>{
+                  setEditId(item.id)
+                  setEditText(item.title)
+                }}>
+                  Edit
+                </button>
+              </>
+            )}
+
+            <Link to={`${item.id}`}>View Details</Link>
+
+            <button onClick={()=>dispatch(deletePost(item.id))}>
+              Delete Post
+            </button>
+
+          </div>
+        ))
+      }
+    </>
   )
 }
 
--- src/features/fetchSlice.js
+++ src/features/fetchSlice.js
@@ -1,24 +1,31 @@
 import { createSlice } from "@reduxjs/toolkit"
 
-const initialState={
-    posts:[]
+const initialState = {
+    posts: []
 }
 
-const fetchSlice=createSlice({
-    name:"fetchData",
+const fetchSlice = createSlice({
+    name: "fetchData",
     initialState,
-    reducers:{
-        displayPosts:(state,action)=>{
-            state.posts=(action.payload);
+    reducers: {
+        displayPosts: (state, action) => {
+            state.posts = (action.payload);
         },
-        deletePost:(state,action)=>{
-            state.posts=state.posts.filter((post)=>post.id!==action.payload)
+        deletePost: (state, action) => {
+            state.posts = state.posts.filter((post) => post.id !== action.payload)
         },
-        addPost:(state,action)=>{
+        addPost: (state, action) => {
             state.posts.unshift(action.payload);
+        },
+        editPost: (state, action) => {
+            const { id, title } = action.payload;
+            const post = state.posts.find((p) => p.id === id);
+            if (post) {
+                post.title = title;
+            }
         }
     }
 })
 
-export const {displayPosts,deletePost,addPost}=fetchSlice.actions;
+export const { displayPosts, deletePost, addPost,editPost} = fetchSlice.actions;
 export default fetchSlice.reducer;
